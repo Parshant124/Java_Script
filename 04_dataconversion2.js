4fe42ba37f4dda97b3e628e3af675ae216e899e5
Parshant124 <parsh3456@gmail.com>
2025-12-23 00:40:02 +0530

    Lecture 7

---
+++ 04_dataconversion2.js
@@ -0,0 +1,15 @@
+let str1 = "Hiii"
+let str2 = " There"
+let str3 = str1 + str2
+
+console.log(str3);        // Hiii There
+
+
+// These topic are only for knowledge not used in real life as code readability matters
+
+console.log("1"+2);        // 12
+console.log(1+"2");        // 12
+console.log("1"+2+3);      // 123
+console.log(1+2+"3");      // 33
+
+console.log(true);
