821e57da37aa6d7f3efea8fd5d483fcbf30fe7ed
Parshant124 <parsh3456@gmail.com>
2025-12-22 11:52:44 +0530

    container config data

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 1234                    // Value cannot be changed
+let accountEmail = "parsh@gmail.com"      // let and var do the same kinda work but let handle the scope resolution thing more accurately so prefer not to use var
+var accountPass = "123456"
+accountCity = "Jaipur"                    // possible to directly initialize but not a good practice
+
+console.log(accountId)
+console.table([accountId,accountEmail,accountPass,accountCity]) // To print multiple variables at once
+
+accountEmail = "par.com"                  // Values can be changed except for const
+accountPass = "1234"
+accountCity = "Mansa"
+
+let accountNo ;                          // only defining
+
+console.log("After changing the values : ")
+console.table([accountId,accountEmail,accountPass,accountCity,accountNo])
