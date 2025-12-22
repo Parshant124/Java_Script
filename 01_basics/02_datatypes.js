15a9f97a2d18da3a4fd9a3594fd0ef9132110c37
Parshant124 <parsh3456@gmail.com>
2025-12-22 12:16:36 +0530

    container config data

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict";     // Syntax to treat the code on the basis of new js
+
+// alert("Any Message") // Use to give the alert window on the browser,(syntax for browser not for node js,uske liye alg syntax h)
+
+// ECMAstandards (ye js ke code format ke standard set krta h , read if you are going to build a website not recommended for casual learning)
+// MDM also do the same
+
+// Data_Types
+
+// Primitive data types
+
+let name = "Parshant"    // string => " "
+let age = 20             // int
+let marks = 99.99        // float
+let present = true       // boolean => true/false
+let state = null ;       // null => standalone value   
+let city;                // undefined 
+// symbol => unique
+
+console.table([typeof "Hiii" ,
+               typeof age ,           // age me int value h to ye number hoga
+               typeof null ,          // null is a object
+               typeof undefined])     // undefined , undefined hi h
