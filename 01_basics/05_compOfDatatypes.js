3c37901ae080669c37f9e7d1faba6766714ffffe
Parshant124 <parsh3456@gmail.com>
2025-12-23 00:54:07 +0530

    lecture 8

---
+++ 01_basics/05_compOfDatatypes.js
@@ -0,0 +1,19 @@
+console.log(2>1);   // true
+
+console.log("5"<6); // true
+// automatically converts the string to number
+
+console.log("5a"<6); //false
+
+console.log(null > 0); // false
+console.log(null==0);  // false
+console.log(null>=0);  // true
+// bcoz operators (> , >= . < , <=) converts null into 0 ; while == don't do so.
+// undefined hr case mein false hi dega
+
+
+// === -> known as strict check
+
+console.log("5"==5)    //true
+console.log("5"===5)   //false
+//In 2nd case it also checks the datatype , as datatype is mismatched one is string and other number so it gives false
