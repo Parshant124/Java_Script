e9aad03821d79cfe6119b611f8520a33c6635e1b
Parshant124 <parsh3456@gmail.com>
2025-12-25 17:25:10 +0530

    lecture 18

---
+++ 02_basics/05_objects.js
@@ -0,0 +1,26 @@
+const user = {
+    name: "Parshant",
+    age: 21,
+    email: "124@gmail.com"
+}
+
+// const {name} = user             // const {key} = Object
+//                                 // From the object we will extract the value stored in the key
+// console.log(name)               // Now for future ref we can use name insted of user.name or user["name"]
+
+// const {email : em} = user       // we can further rename it...
+// console.log(em)
+// // name = "AA"                     // Error
+// user.name = "AA"
+// console.log(name)
+
+
+// API :- In short apne kaam ko kisi aur pr daalna
+
+// {
+//     "name": "Parshant",
+//     "course": "Chai aur Code",
+//     "coding": "JavaScript"
+// }
+
+// looks like an object, keys ko as a string implement krna pdega and iska koi variable name nhi hoga
