9be4f77b6efb3953b07427d094bf6a75e3d16b88
Parshant124 <parsh3456@gmail.com>
2025-12-28 13:03:20 +0530

    lecture 26

---
+++ 04_control_flow.js/01_if_else.js
@@ -0,0 +1,30 @@
+// simple if else hi hai, and same type for switch case
+
+// falsy values
+// 0, -0, BigInt 0n, "", false, null, undefined, NaN
+
+// truthy values
+// [], "0", "false", {}, function() {}, 
+
+
+// Nullish Coalescing Operator (??): null undefined ke liye use hota h
+
+// let val1;
+// val1 = 5 ?? 10
+// console.log(val1)       // 5
+
+// let val2;
+// val2 = null ?? 10
+// console.log(val2)       // 10
+
+// let val3;
+// console.log(val3)           // undefined
+// console.log(val3??5)        // 5
+// console.log(val3??null??undefined??15??10)  // 15
+
+// Use kyu hota h : agr kisi variable mein null ya undefined hai to vo aage error cause kr skta h
+// usse bchne ke liye, ?? operator ka use hota h.
+
+// Ternary operator ( ? : )
+
+// condition ? true : false
