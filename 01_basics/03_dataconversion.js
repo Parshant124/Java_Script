73b2bed57582e7178297664f48fb8eddf52d40db
Parshant124 <parsh3456@gmail.com>
2026-01-09 23:39:14 +0530

    "" to Number

--- 01_basics/03_dataconversion.js
+++ 01_basics/03_dataconversion.js
@@ -1,15 +1,19 @@
 let score = undefined
 
 let convers = Number(score)
-console.log(typeof(score))
-console.log(score)
-console.log(typeof(convers))  // typeof(a) and typeof a is same
-console.log(convers)          
+// console.log(typeof(score))
+// console.log(score)
+// console.log(typeof(convers))  // typeof(a) and typeof a is same
+// console.log(convers)          
+
+let str = ""
+console.log(Number(str))
 
 //"50"          string     =>  50    number   string contained the pure number
 //"5a"          string     =>  NaN   number   Nan stands for Not a Number , but its type is a number
 //null          object     =>  0     number
 //undefined     undefined  =>  NaN   number
 //true          boolean    =>  1     number
+//""            string     =>  0     number
 //""            string     =>  false boolean   Empty string is considered as false
 //"Any_sentece" string     =>  true  boolean   String with elements is considered as true
