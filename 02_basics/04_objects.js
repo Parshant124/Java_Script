cec7a07ffe1ba2d05d6734c508107544159aee5d
Parshant124 <parsh3456@gmail.com>
2026-01-10 00:25:57 +0530

    comment changed

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -29,7 +29,7 @@ const other_User = {
 const obj1 = {'a': 1, 'b': 2};
 const obj2 = {'c': 3, 'd': 4};
 
-// const obj3 = Object.assign(obj1, obj2)                  // Here target object in obj1, i.e. obj1 is modified    
+// const obj3 = Object.assign(obj1, obj2)                  // Here target object is obj1, i.e. obj1 is modified    
 // console.log(obj3)                                       // { a: 1, b: 2, c: 3, d: 4 }
 // console.log(obj1)                                       // { a: 1, b: 2, c: 3, d: 4 }
 
